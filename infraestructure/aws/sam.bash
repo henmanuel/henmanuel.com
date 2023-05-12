78d39fe904f94e2d1a3c29299acd74745056985b
henmanuel <henmanuelvargas@gmail.com>
2023-05-12 14:14:23 -0600

    Start personal brand

--- infraestructure/aws/sam.bash
+++ infraestructure/aws/sam.bash
@@ -1,13 +1,12 @@
-echo "Enter domain app: "
-read APPName
+echo "Enter domain app: example.com"
+read APPDomain
 
-if [ -z "$APPName" ]
-then
-      echo "Domain app is required"
-      exit 1
+if [ -z "$APPDomain" ]; then
+  echo "Domain app is required"
+  exit 1
 fi
 
-echo "Enter the name of environment: Default -> dev"
+echo "Enter the name of Environment: Default -> dev || qa || prod"
 read Environment
 
 Dot="."
@@ -19,9 +18,6 @@ elif [[ $Environment == "qa" ]]; then
   echo "The option chosen is qa"
 elif [[ $Environment == "prod" ]]; then
   echo "The option chosen is prod"
-  Environment=""
-  Hyphen=""
-  Dot=""
 else
   echo "The option chosen is not valid, the default option is dev"
   Environment="dev"
@@ -30,17 +26,23 @@ fi
 echo "Enter the name of the region: Default -> us-east-1"
 read Region
 
-if [ -z "$Region" ]
-then
-      Region="us-east-1"
+if [ -z "$Region" ]; then
+  Region="us-east-1"
 fi
 
-StackName=${APPName/./-}
-TemplateName=${APPName}.template
-BucketName=${Environment}${Dot}${APPName}-deploy
-DeploymentBucketName=${Environment}${Dot}${APPName}-deploy
+TemplateName=${APPDomain}.template
+StackName=${Environment}${Hyphen}${APPDomain/./-}
+
+if [[ $Environment == "prod" ]]; then
+  Environment=""
+  Hyphen=""
+  Dot=""
+fi
+
+Environment="${Environment}${Dot}"
+BucketName=${Environment}${APPDomain}-deploy
 
 brew install awscli
 aws s3api create-bucket --bucket "$BucketName" --region us-east-1
-sam deploy -t "$TemplateName" --stack-name "${Environment}${Hyphen}${StackName}" --s3-bucket "$DeploymentBucketName" --region $Region --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM --parameter-overrides AppName="$APPName" Environment="$Environment" Region="$Region"
-aws s3 sync ../../src s3://"${Environment}${Dot}${APPName}"
+sam deploy -t "$TemplateName" --stack-name "$StackName" --s3-bucket "$BucketName" --region $Region --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM --parameter-overrides AppName="$APPDomain" Environment="$Environment" Region="$Region"
+aws s3 sync ../../src s3://"${Environment}${APPDomain}"
